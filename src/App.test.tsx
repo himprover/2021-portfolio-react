4d1c6c00eb4fff3399a6efa5d5a2b556a93605f4
himprover <hwang.improver@gmail.com>
2021-11-25 22:30:48 +0900

    hotfix: setup branch recover

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
