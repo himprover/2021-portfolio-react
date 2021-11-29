534ad8e9f263e989e79f83ed00cfb1e99ee841b9
himprover <hwang.improver@gmail.com>
2021-11-30 01:17:00 +0900

    add: Content.tsx_Content Scroll Y offset searching

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -9,14 +9,42 @@ import { useEffect, useState, useRef } from 'react';
 function Content() {
 	const mainRef = useRef<HTMLDivElement>(null);
 	const skillsRef = useRef<HTMLDivElement>(null);
+	const referenceRef = useRef<HTMLDivElement>(null);
+	const hobbyRef = useRef<HTMLDivElement>(null);
+	const contactRef = useRef<HTMLDivElement>(null);
+
+	const [scrollY, setScrollY] = useState<number>(0);
+	const [section, setSection] = useState<number>(0);
+
+	const scrollHandler = (e: React.UIEvent<HTMLDivElement>) => {
+		let deltaY = scrollY - e.currentTarget.scrollTop;
+		if (deltaY < 0) {
+			console.log('내림');
+			if (section < 4) {
+				setSection((current) => current + 1);
+			}
+			console.log(section);
+		} else {
+			console.log('올림');
+			if (section > 0) {
+				setSection((current) => current - 1);
+			}
+			console.log(section);
+		}
+		setScrollY(e.currentTarget.scrollTop);
+	};
+
+	// const scrollHandler = (e: React.UIEvent<HTMLDivElement>) => {
+	// 	console.log(e.target);
+	// };
 
 	return (
-		<ContentDIV>
+		<ContentDIV onScroll={scrollHandler}>
 			<Main ref={mainRef} />
 			<Skills ref={skillsRef} />
-			<Reference />
-			<Hobby />
-			<Contact />
+			<Reference ref={referenceRef} />
+			<Hobby ref={hobbyRef} />
+			<Contact ref={contactRef} />
 		</ContentDIV>
 	);
 }
