6638db6afdae864622319dbcf802d3ada246efd6
himprover <hwang.improver@gmail.com>
2021-11-29 17:50:41 +0900

    change: Content.tsx_need to fullpage scrolling func

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -6,6 +6,16 @@ import Hobby from './Contents/Hobby';
 import Contact from './Contents/Contact';
 import { useEffect, useState, useRef } from 'react';
 
+const throttle = function (callback: any, waitTime: any) {
+	let timerId: any = null;
+	return (e: any) => {
+		if (timerId) return;
+		timerId = setTimeout(() => {
+			timerId = null;
+		}, waitTime);
+	};
+};
+
 function Content() {
 	const mainRef = useRef<HTMLDivElement>(null);
 	const skillsRef = useRef<HTMLDivElement>(null);
