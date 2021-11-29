04c650e59b266323e8b14e810bc2a825c270bd1e
himprover <hwang.improver@gmail.com>
2021-11-29 14:23:21 +0900

    add: Header-Content layout

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -8,6 +8,9 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     margin: 0;
+	padding-left: 5rem;
+	overflow-x:hidden;
+	overflow-y:scroll;
 	background: ${({ theme }: { theme: any }) => theme.bgColor};
 	color: ${({ theme }: { theme: any }) => theme.fontColor};
 	font-family: 'IBMPlexSansKR', sans-serif;
