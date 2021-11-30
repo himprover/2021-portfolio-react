7d4f73142a3db45a26c091d49d2304c6a5e1e112
himprover <hwang.improver@gmail.com>
2021-11-30 23:32:06 +0900

    add: Main.tsx_Text Area

--- src/pages/Contents/Main.tsx
+++ src/pages/Contents/Main.tsx
@@ -4,16 +4,28 @@ import { forwardRef } from 'react';
 const Main = forwardRef<any>((props, ref) => {
 	return (
 		<MainDIV ref={ref}>
-			<LeftDIV></LeftDIV>
-			<RightDIV></RightDIV>
+			<TextDIV>
+				<Hello>Hello</Hello>
+				<ImHwang>I'm Hwang</ImHwang>
+				<FE>
+					FRONT
+					<br />
+					Developer
+				</FE>
+			</TextDIV>
+			<ImgDIV></ImgDIV>
 		</MainDIV>
 	);
 });
 
 const MainDIV = styled.div``;
 
-const LeftDIV = styled.div``;
-const RightDIV = styled.div``;
+const TextDIV = styled.div``;
+const Hello = styled.h2``;
+const ImHwang = styled.h1``;
+const FE = styled.h1``;
+
+const ImgDIV = styled.div``;
 
 const Img = styled.img``;
 
