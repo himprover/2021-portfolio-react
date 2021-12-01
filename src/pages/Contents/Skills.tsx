9d14398f9881a8a7bdc3da61cbc95526d55c94d5
himprover <hwang.improver@gmail.com>
2021-12-01 15:24:45 +0900

    change: folder detail add, png file add

--- src/pages/Contents/Skills.tsx
+++ src/pages/Contents/Skills.tsx
@@ -1,13 +1,34 @@
 import styled from 'styled-components';
 import { forwardRef } from 'react';
 
+import { ReactComponent as HTML5SVG } from '../../imgs/skills/svg/HTML5.svg';
+
 const Skills = forwardRef<any>((props, ref) => {
-	return <SkillsDIV ref={ref}></SkillsDIV>;
+	return (
+		<SkillsDIV ref={ref}>
+			<Title>Skills</Title>
+			<HTML5 />
+		</SkillsDIV>
+	);
 });
 
 const SkillsDIV = styled.div`
 	background: ${({ theme }: { theme: any }) => theme.skillsBgColor};
 	overflow: hidden;
+`;
+
+const Title = styled.h1`
+	font-size: 6rem;
+	font-weight: 400;
+	position: absolute;
+	margin: 0;
+	padding-left: 3rem;
+	padding-top: 3rem;
+	top: 0;
+`;
+
+const HTML5 = styled(HTML5SVG)`
+	position: absolute;
 `;
 
 export default Skills;
