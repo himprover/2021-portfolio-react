b5d95bb7a9be6481318a8b4ab5d0ced89c442dd0
himprover <hwang.improver@gmail.com>
2021-12-03 13:08:58 +0900

    add: Hobby_title

--- src/pages/Contents/Hobby.tsx
+++ src/pages/Contents/Hobby.tsx
@@ -2,9 +2,28 @@ import styled from 'styled-components';
 import { forwardRef } from 'react';
 
 const Hobby = forwardRef<any>((props, ref) => {
-	return <HobbyDIV ref={ref}></HobbyDIV>;
+	return (
+		<HobbyDIV ref={ref}>
+			<Title>Hobby</Title>
+		</HobbyDIV>
+	);
 });
 
-const HobbyDIV = styled.div``;
+const HobbyDIV = styled.div`
+	background: radial-gradient(
+		82.47% 260.66% at 16.85% 50%,
+		#474747 0%,
+		#2e2e2e 20.83%,
+		#212121 100%
+	);
+`;
+
+const Title = styled.h1`
+	font-size: 6rem;
+	font-weight: 400;
+	margin: 0;
+	padding-left: 3rem;
+	padding-top: 3rem;
+`;
 
 export default Hobby;
