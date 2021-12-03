2fff4336cdc04e6d026a0f94f85b8eaeecbfc5a2
himprover <hwang.improver@gmail.com>
2021-12-04 00:48:00 +0900

    add: Content.tsx_Full Paging with Throttle

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Main from './Contents/Main';
 import Skills from './Contents/Skills';
 import Reference from './Contents/Reference';
 import Hobby from './Contents/Hobby';
 import Contact from './Contents/Contact';
 
-import { useEffect, useState, useRef, useMemo } from 'react';
 import { throttle } from 'lodash';
+
+import { useEffect, useState, useRef, useMemo, useCallback } from 'react';
 
 function Content() {
 	const contentRef = useRef<HTMLDivElement>(null);
@@ -16,55 +17,53 @@ function Content() {
 	const hobbyRef = useRef<HTMLDivElement>(null);
 	const contactRef = useRef<HTMLDivElement>(null);
 
-	//const [section, setSection] = useState<number>(0);
-
 	const section = useRef<number>(0);
 
-	const onScroll = throttle((e: React.UIEvent<HTMLDivElement>) => {
-		if (e.currentTarget !== null) {
-			let yArray = [
-				mainRef.current!.offsetTop,
-				skillsRef.current!.offsetTop,
-				referenceRef.current!.offsetTop,
-				hobbyRef.current!.offsetTop,
-				contactRef.current!.offsetTop,
-			];
-			let scrollY = e.currentTarget.scrollTop;
-			console.log(yArray[section.current] as number);
-			console.log(scrollY);
-			let deltaY = (yArray[section.current] as number) - scrollY;
-			console.log('스크롤 이벤트');
-			if (deltaY < 0) {
-				console.log('내림');
-				if (section.current < 4) {
-					section.current++;
-					return contentRef.current!.scrollTo({
-						top: yArray[section.current],
-						behavior: 'smooth',
-					});
-				}
-			} else {
-				console.log('올림');
-				if (section.current > 0) {
-					section.current--;
-					return contentRef.current!.scrollTo({
-						top: yArray[section.current],
-						behavior: 'smooth',
-					});
-				}
-			}
-		}
-	}, 290);
+	const [sectionState, setSection] = useState<number>(0);
 
-	// useEffect(() => {
-	// 	window.addEventListener('scroll', () => {});
-	// 	return () => {
-	// 		window.removeEventListener('scroll', () => {});
-	// 	};
-	// }, []);
+	const scrollHandler = useMemo(
+		() =>
+			throttle((e) => {
+				if (e.deltaY < 0) {
+					if (section.current > 0) {
+						console.log('??');
+						setSection((current) => current - 1);
+						section.current--;
+					}
+				} else {
+					if (section.current < 4) {
+						console.log('??');
+						setSection((current) => current + 1);
+						section.current++;
+					}
+				}
+			}, 1000),
+		[section]
+	);
+
+	useEffect(() => {
+		window.addEventListener('wheel', scrollHandler);
+		return () => {
+			window.removeEventListener('wheel', scrollHandler);
+		};
+	}, [scrollHandler]);
+
+	// return () => {
+	// 	window.removeEventListener('wheel', (e) => {
+	// 		if (e.deltaY < 0) {
+	// 			if (section > 0) {
+	// 				setSection((current) => current - 1);
+	// 			}
+	// 		} else {
+	// 			if (section < 4) {
+	// 				setSection((current) => current + 1);
+	// 			}
+	// 		}
+	// 	});
+	// };
 
 	return (
-		<ContentDIV ref={contentRef} onScroll={onScroll}>
+		<ContentDIV ref={contentRef} nowSection={sectionState}>
 			<Main ref={mainRef} />
 			<Skills ref={skillsRef} />
 			<Reference ref={referenceRef} />
@@ -74,9 +73,16 @@ function Content() {
 	);
 }
 
-const ContentDIV = styled.div`
-	overflow-y: scroll;
+const ContentDIV = styled.div<{ nowSection: number }>`
+	overflow: visible;
+	position: fixed;
 	height: 100vh;
+	top: 0;
+	transition: all 1000ms ease 0s;
+	${(props) =>
+		css`
+			transform: translateY(${-100 * props.nowSection + 'vh'});
+		`};
 	& > div {
 		height: 100vh;
 		position: relative;
