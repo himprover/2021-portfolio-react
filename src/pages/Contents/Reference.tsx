3d37cf47f942f4a9f16f5a10241916fad92a810b
himprover <hwang.improver@gmail.com>
2021-12-03 11:46:59 +0900

    add: Reference.tsx_Arrow

--- src/pages/Contents/Reference.tsx
+++ src/pages/Contents/Reference.tsx
@@ -1,13 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { forwardRef } from 'react';
 
 import { ReactComponent as LightSVG } from '../../imgs/reference/svg/light.svg';
+import { ReactComponent as ArrowSVG } from '../../imgs/reference/svg/arrow.svg';
 
 const Reference = forwardRef<any>((props, ref) => {
 	return (
 		<ReferenceDIV ref={ref}>
 			<Title>Skills</Title>
-			<ListDIV></ListDIV>
+
+			<ListDIV>
+				<ArrowDIV>
+					<Arrow direction='right' />
+				</ArrowDIV>
+				<List />
+				<ArrowDIV>
+					<Arrow direction='left' />
+				</ArrowDIV>
+			</ListDIV>
+
 			<Light />
 		</ReferenceDIV>
 	);
@@ -28,10 +39,15 @@ const Title = styled.h1`
 `;
 
 const ListDIV = styled.div`
-	width: 85vw;
+	display: flex;
+	width: 100%;
 	height: 70vh;
+`;
+
+const List = styled.div`
+	background: #979797;
+	width: 80vw;
 	margin: 0 auto;
-	background: #9b9b9b;
 `;
 
 const Light = styled(LightSVG)`
@@ -41,6 +57,25 @@ const Light = styled(LightSVG)`
 	width: 40rem;
 	transform: rotate(180deg) translateX(50%);
 	filter: drop-shadow(0px 10px 40px white);
+`;
+
+const ArrowDIV = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	width: 10vw;
+`;
+
+const Arrow = styled(ArrowSVG)`
+	width: 3rem;
+	display: block;
+	cursor: pointer;
+	${(props) =>
+		props.direction === 'right'
+			? css`
+					transform: rotate(180deg);
+			  `
+			: ''};
 `;
 
 export default Reference;
