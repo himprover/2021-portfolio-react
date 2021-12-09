33e9feec6162af1eaa62ebbc1e2ad523680d6219
himprover <hwang.improver@gmail.com>
2021-12-09 17:34:26 +0900

    add: Main.tsx_Animation without Line

--- src/pages/Contents/Main.tsx
+++ src/pages/Contents/Main.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { forwardRef } from 'react';
 
 import Hwang from 'imgs/main/Hwang.png';
@@ -6,18 +6,21 @@ import { ReactComponent as Polygon1 } from 'imgs/main/polygon1.svg';
 import { ReactComponent as Polygon2 } from 'imgs/main/polygon2.svg';
 
 import RedGreenLine from 'components/RedGreenLine';
+import { useSelector } from 'react-redux';
+import { RootState } from 'modules';
 
 const Main = forwardRef<any>((props, ref) => {
+	const count = useSelector((state: RootState) => state.counter.count);
 	return (
 		<MainDIV ref={ref}>
 			<RedGreenLine />
-			<Hello>Hello</Hello>
-			<ImHwang>I'm Hwang</ImHwang>
-			<FETOP>FRONT</FETOP>
-			<FEBOTTOM>Developer</FEBOTTOM>
-			<Polygon1Styled />
-			<Polygon2Styled />
-			<HwangStyled src={Hwang} />
+			<Hello nowSection={count}>Hello</Hello>
+			<ImHwang nowSection={count}>I'm Hwang</ImHwang>
+			<FETOP nowSection={count}>FRONT</FETOP>
+			<FEBOTTOM nowSection={count}>Developer</FEBOTTOM>
+			<Polygon1Styled nowSection={count} />
+			<Polygon2Styled nowSection={count} />
+			<HwangStyled src={Hwang} nowSection={count} />
 		</MainDIV>
 	);
 });
@@ -26,22 +29,68 @@ const MainDIV = styled.div`
 	background: ${({ theme }: { theme: any }) => theme.mainBgColor};
 `;
 
-const fadeInFE1 = keyframes`
+const PolyFadeIn = keyframes`
 	0% {
-		transform:translate(100rem, -100rem) rotate(-45deg);
+		opacity:0;
+		transform-origin : 0 100%;
+		transform:rotate(-15deg);
+	} 1%{ 
+		opacity:1;
 	} 100% {
-		transform:translate(0, 0) rotate(-45deg);
-	}
-`;
-const fadeInFE2 = keyframes`
-	0% {
-		transform:translate(-100rem, 100rem) rotate(-45deg);
-	} 100% {
-		transform:translate(0, 0) rotate(-45deg);
+		opacity:1;
+		transform-origin : 0 100%;
+		transform:rotate(0);
 	}
 `;
 
-const Hello = styled.h2`
+const fadeIn = keyframes`
+	0% {
+		opacity:0;
+	} 100% {
+		opacity:1;
+	}
+`;
+
+const fadeOut = keyframes`
+	0% {
+		opacity:1;
+	} 100% {
+		opacity:0;
+	}
+`;
+
+const rightIn = keyframes`
+	0% {
+		transform:translate(200rem, -200rem) rotate(-45deg);
+	} 100% {
+		transform:translate(0, 0) rotate(-45deg);
+	}
+`;
+const rightOut = keyframes`
+	0% {
+		transform:translate(0, 0) rotate(-45deg);
+	} 100% {
+		transform:translate(200rem, -200rem) rotate(-45deg);
+	}
+`;
+const leftIn = keyframes`
+	0% {
+		transform:translate(-200rem, 200rem) rotate(-45deg);
+	} 100% {
+		transform:translate(0, 0) rotate(-45deg);
+	}
+`;
+const leftOut = keyframes`
+	0% {
+		transform:translate(0, 0) rotate(-45deg);
+	} 100% {
+		transform:translate(-200rem, 200rem) rotate(-45deg);
+		
+	}
+`;
+
+const Hello = styled.h2<{ nowSection: number }>`
+	opacity: 0;
 	position: absolute;
 	transform: rotate(-45deg);
 	font-size: 6rem;
@@ -50,8 +99,18 @@ const Hello = styled.h2`
 	left: 7rem;
 	color: ${({ theme }: { theme: any }) => theme.mainHelloColor};
 	text-shadow: ${({ theme }: { theme: any }) => theme.mainTextShadow};
+
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${fadeIn} 1s 1.2s forwards;
+			  `
+			: css`
+					animation: ${fadeOut} 1s backwards;
+			  `}
 `;
-const ImHwang = styled.h1`
+const ImHwang = styled.h1<{ nowSection: number }>`
+	opacity: 0;
 	position: absolute;
 	transform: rotate(-45deg);
 	font-size: 9rem;
@@ -60,6 +119,14 @@ const ImHwang = styled.h1`
 	left: -1rem;
 	color: ${({ theme }: { theme: any }) => theme.mainImHColor};
 	text-shadow: ${({ theme }: { theme: any }) => theme.mainTextShadow};
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${fadeIn} 1s 1.5s forwards;
+			  `
+			: css`
+					animation: ${fadeOut} 1s backwards;
+			  `}
 `;
 const FE = styled.h1`
 	position: absolute;
@@ -71,46 +138,90 @@ const FE = styled.h1`
 	text-shadow: ${({ theme }: { theme: any }) => theme.mainTextShadow};
 `;
 
-const FETOP = styled(FE)`
+const FETOP = styled(FE)<{ nowSection: number }>`
 	top: -4rem;
 	left: -12rem;
-	animation: ${fadeInFE1} 1s;
+	transform: translate(200rem, -200rem) rotate(-45deg);
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${rightIn} 1s 1.7s forwards;
+			  `
+			: css`
+					animation: ${rightOut} 1s forwards;
+			  `}
 `;
 
-const FEBOTTOM = styled(FE)`
+const FEBOTTOM = styled(FE)<{ nowSection: number }>`
 	top: -2rem;
 	left: 0rem;
-	animation: ${fadeInFE2} 1s;
+
+	transform: translate(-200rem, 200rem) rotate(-45deg);
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${leftIn} 1s 1.7s forwards;
+			  `
+			: css`
+					animation: ${leftOut} 1s forwards;
+			  `}
 `;
 
-const Polygon1Styled = styled(Polygon1)`
+const Polygon1Styled = styled(Polygon1)<{ nowSection: number }>`
 	position: absolute;
 	right: 30rem;
 	top: 20rem;
 	width: 65.7rem;
 	z-index: 1;
+	opacity: 0;
 	filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.6));
 	path {
 		fill: ${({ theme }: { theme: any }) => theme.mainP1Color};
 	}
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${fadeIn} 1s 2s forwards;
+			  `
+			: css`
+					animation: ${fadeOut} 1s backwards;
+			  `}
 `;
-const Polygon2Styled = styled(Polygon2)`
+const Polygon2Styled = styled(Polygon2)<{ nowSection: number }>`
 	position: absolute;
 	right: 27.9rem;
 	top: 33rem;
 	width: 55rem;
 	z-index: 0;
+	opacity: 0;
 	filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.6));
 	path {
 		fill: ${({ theme }: { theme: any }) => theme.mainP2Color};
 	}
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${PolyFadeIn} 1s 2.7s forwards;
+			  `
+			: css`
+					animation: ${fadeOut} 1s backwards;
+			  `}
 `;
-const HwangStyled = styled.img`
+const HwangStyled = styled.img<{ nowSection: number }>`
 	position: absolute;
 	right: 32.1rem;
 	top: -1rem;
 	z-index: 3;
+	opacity: 0;
 	filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.6));
+	${(props) =>
+		props.nowSection === 0
+			? css`
+					animation: ${fadeIn} 1s 2s forwards;
+			  `
+			: css`
+					animation: ${fadeOut} 1s backwards;
+			  `}
 `;
 
 export default Main;
