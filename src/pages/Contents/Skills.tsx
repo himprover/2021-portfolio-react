0321f8ce45e31570816c8f1d1de7a22cd54fca7a
himprover <hwang.improver@gmail.com>
2021-12-13 10:32:35 +0900

    add: skills.tsx_intro & outro moving animation

--- src/pages/Contents/Skills.tsx
+++ src/pages/Contents/Skills.tsx
@@ -1,5 +1,7 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { forwardRef } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'modules';
 
 import { ReactComponent as HTML5SVG } from '../../imgs/skills/svg/HTML5.svg';
 import { ReactComponent as ES6SVG } from '../../imgs/skills/svg/ES6.svg';
@@ -10,16 +12,17 @@ import { ReactComponent as NODESVG } from '../../imgs/skills/svg/NODE.svg';
 import { ReactComponent as PHPSVG } from '../../imgs/skills/svg/PHP.svg';
 
 const Skills = forwardRef<any>((props, ref) => {
+	const count = useSelector((state: RootState) => state.counter.count);
 	return (
 		<SkillsDIV ref={ref}>
 			<Title>Skills</Title>
-			<Logo1DIV>
+			<Logo1DIV nowsection={count}>
 				<HTML5 />
 				<ES6 />
 				<REACT />
 				<TS />
 			</Logo1DIV>
-			<Logo2DIV>
+			<Logo2DIV nowsection={count}>
 				<NEST />
 				<NODE />
 				<PHP />
@@ -27,6 +30,35 @@ const Skills = forwardRef<any>((props, ref) => {
 		</SkillsDIV>
 	);
 });
+
+const rightMove = keyframes`
+	from {
+		transform:translateX(-100vw);
+	} to {
+		transform:translateX(0);
+	}
+`;
+const rightMoveUndo = keyframes`
+	from {
+		transform:translateX(0);
+	} to {
+		transform:translateX(-100vw);
+	}
+`;
+const leftMove = keyframes`
+	from {
+		transform:translateX(100vw);
+	} to {
+		transform:translateX(0);
+	}
+`;
+const leftMoveUndo = keyframes`
+	from {
+		transform:translateX(0);
+	} to {
+		transform:translateX(100vw);
+	}
+`;
 
 const SkillsDIV = styled.div`
 	background: ${({ theme }: { theme: any }) => theme.skillsBgColor};
@@ -50,21 +82,39 @@ const SkillsDIV = styled.div`
 	}
 `;
 
-const Logo1DIV = styled.div`
+const Logo1DIV = styled.div<{ nowsection: number }>`
 	display: flex;
 	justify-content: space-between;
 	width: 170rem;
 	margin: 0 auto;
+	z-index: 2;
+	${(props) =>
+		props.nowsection === 1
+			? css`
+					animation: ${rightMove} 1s forwards;
+			  `
+			: css`
+					animation: ${rightMoveUndo} 1s forwards;
+			  `}
 	polygon {
 		fill: #64646465;
 		stroke: #ffffff;
 	}
 `;
-const Logo2DIV = styled.div`
+const Logo2DIV = styled.div<{ nowsection: number }>`
 	display: flex;
 	justify-content: space-between;
 	width: 110rem;
 	margin: 0 auto;
+	z-index: 2;
+	${(props) =>
+		props.nowsection === 1
+			? css`
+					animation: ${leftMove} 1s forwards;
+			  `
+			: css`
+					animation: ${leftMoveUndo} 1s forwards;
+			  `}
 	rect {
 		fill: #ffffff64;
 		stroke: #ffffff;
@@ -84,6 +134,7 @@ const HTML5 = styled(HTML5SVG)`
 	width: 30rem;
 	transform: rotate(15deg);
 	filter: drop-shadow(42px 46px 0px rgba(0, 0, 0, 0.25));
+
 	z-index: 2;
 	overflow: hidden;
 
@@ -97,6 +148,7 @@ const ES6 = styled(ES6SVG)`
 	transform: rotate(15deg);
 	filter: drop-shadow(42px 46px 0px rgba(0, 0, 0, 0.25));
 	z-index: 2;
+
 	img {
 		z-index: 2;
 	}
