20ccb970ae3458715fbb2d5d36c127d8d5d7a9d8
himprover <hwang.improver@gmail.com>
2021-12-19 01:51:15 +0900

    Reference responsive

--- src/pages/Contents/Reference.tsx
+++ src/pages/Contents/Reference.tsx
@@ -1,7 +1,7 @@
 import styled, { css, keyframes } from 'styled-components';
 import { useSelector } from 'react-redux';
 import { RootState } from 'modules';
-import { useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { ReactComponent as LightSVG } from 'imgs/reference/svg/light.svg';
 import { ReactComponent as ArrowSVG } from 'imgs/reference/svg/arrow.svg';
@@ -12,12 +12,15 @@ import Four from 'components/References/Four';
 import Five from 'components/References/Five';
 import Six from 'components/References/Six';
 import Seven from 'components/References/Seven';
+import { throttle } from 'lodash';
 function Reference() {
 	const nowsection = useSelector(
 		(state: RootState) => state.sectionHandle.nowsection
 	);
 	const [slide, setSlide] = useState<number>(0);
 	const [isoff, setOff] = useState<boolean>(false);
+
+	const vw = useRef(0);
 
 	const slideHandle = (direction: string) => {
 		if (direction === 'right') {
@@ -108,7 +111,7 @@ const ReferenceDIV = styled.div<{ nowsection: number }>`
 	align-items: center;
 	top: 0;
 	z-index: 10;
-	width: 1870px;
+	width: calc(100vw - 5rem);
 	height: 100vh;
 	transition: all 0.5s;
 	${(props) =>
--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -3,12 +3,7 @@ import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
 	${normalize}
-	html{
-		font-size:10px;
-	}
-	/* @media screen and (max-width:1680px) {
-		html{font-size:8px;}
-	} */
+
 	body {
 	margin: 0;
 	padding-left: 5rem;
@@ -23,9 +18,82 @@ const GlobalStyle = createGlobalStyle`
 	}
 	::-webkit-scrollbar { display: none; }
 	* {
-	box-sizing: border-box;
+	box-sizing: border-box;	// padding&border in width, height
 	transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out, text-shadow 0.5s ease-in-out, fill 0.5s ease-in-out;
 	}
+
+	/* Extra small devices (phones, 600px and down) */
+	@media only screen and (max-width: 600px) {
+
+	}
+	/* Small devices (portrait tablets and large phones, 600px and up) */
+	@media only screen and (min-width: 600px) {
+
+	}
+	/* Medium devices (landscape tablets, 768px and up) */
+	@media only screen and (min-width: 768px) {
+
+	}
+	/* Large devices (laptops/desktops, 992px and up) */
+	@media only screen and (min-width: 992px) {
+
+	}
+		/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1000px) {
+		html{
+			font-size:34%;
+		}
+		
+		/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1100px) {
+		html{
+			font-size:37%;
+		}
+		
+
+		/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1200px) {
+		html{
+			font-size:40%;
+		}
+		
+		/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1300px) {
+		html{
+			font-size:46%;
+		}
+		
+	/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1400px) {
+		html{
+			font-size:49%;
+		}
+		 
+	}
+
+	@media only screen and (min-width: 1500px) {
+		html{
+			font-size:52%;
+		}
+	}
+	/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1600px) {
+		html{
+			font-size:54%;
+		}
+	}
+	/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1700px) {
+		html{
+			font-size:58%;
+		}
+	}
+	@media only screen and (min-width: 1800px) {
+		html{
+			font-size:62.5%;
+		}
+	}
+
 
 	@font-face {
 		font-family: 'IBMPlexSansKR';
