a6ad12dc7d1cace1cf459df19f3660b762d87c2b
himprover <hwang.improver@gmail.com>
2021-12-23 14:57:15 +0900

    hotfix: main update

--- src/pages/Contents/Main.tsx
+++ src/pages/Contents/Main.tsx
@@ -65,10 +65,7 @@ const PolyFadeIn = keyframes`
 		opacity:0;
 		transform-origin : 0 100%;
 		transform:rotate(-15deg);
-		@media only screen and (max-width: 512px) {
-		transform: rotate(-15deg) translateX(30rem);
-		}
-	}
+
 		
 	} 1%{ 
 		opacity:1;
@@ -76,11 +73,23 @@ const PolyFadeIn = keyframes`
 		opacity:1;
 		transform-origin : 0 100%;
 		transform:rotate(0);
-		@media only screen and (max-width: 512px) {
-		transform: rotate(0) translateX(30rem);
-		}
+
 	}
 `;
+
+const PolyFadeInMedia = keyframes`
+	0% {
+		opacity:0;
+		transform-origin : 0 100%;
+		transform: rotate(-15deg) translateX(30rem);
+	}
+	 1%{ 
+		opacity:1;
+	} 100% {
+		opacity:1;
+		transform-origin : 0 100%;
+		transform: rotate(0) translateX(30rem);
+	}`;
 
 const fadeIn = keyframes`
 	0% {
@@ -256,13 +265,17 @@ const Polygon2Styled = styled(Polygon2)<{
 	path {
 		fill: ${({ theme }: { theme: any }) => theme.mainP2Color};
 	}
-	@media only screen and (max-width: 512px) {
-		transform: translateX(30rem);
-	}
+
 	${(props) =>
 		props.nowsection === 0 && !props.isloading
 			? css`
-					animation: ${PolyFadeIn} 1s 2.5s forwards;
+					@media only screen and (max-width: 512px) {
+						transform: translateX(30rem);
+						animation: ${PolyFadeInMedia} 1s 2.5s forwards;
+					}
+					@media only screen and (min-width: 513px) {
+						animation: ${PolyFadeIn} 1s 2.5s forwards;
+					}
 			  `
 			: css`
 					animation: ${fadeOut} 1s backwards;
